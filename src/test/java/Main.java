e2d3fac0a5e2ccbfdbdd1c66d71981baf0a668dc
torstenleibrich <torsten.leibrich@gmail.com>
2017-03-07 21:55:53 +0800

    Adding first todo item

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,13 +1,32 @@
+import org.junit.AfterClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class Main {
     private static WebDriver driver = new FirefoxDriver();
 
-    @Test
-    public void main() throws Exception {
-        driver.get("http://test-automation-dojo.com/");
+    @AfterClass
+    public static void tearDown(){
         driver.quit();
+    }
+
+    @Test
+    public void shouldAddToDos() throws Exception {
+        driver.get("http://test-automation-dojo.com/");
+        WebElement toDo = driver.findElement(By.xpath("//input"));
+        toDo.sendKeys("Let's get started!");
+        toDo.sendKeys(Keys.RETURN);
+        assertThat(currentCounter(), equalTo("1"));
+    }
+
+    private String currentCounter() {
+        return driver.findElement(By.xpath("//footer/span/strong")).getText();
     }
 }
