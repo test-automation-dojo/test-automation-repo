d82fddb89ec05157be426e5002b034782e1368c5
Adam Woods <adam.tomato@gmail.com>
2017-03-09 23:39:29 +0800

    updated test to handle the login page

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
@@ -19,8 +21,18 @@ public class Main {
 
     @Test
     public void shouldAddToDos() throws Exception {
-        driver.get("http://test-automation-dojo.com/");
-        WebElement toDo = driver.findElement(By.xpath("//input"));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        driver.get("http://test-automation-dojo.com/todo");
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement signIn = driver.findElement(By.id("sign-in"));
+
+        username.sendKeys("toby");
+        password.sendKeys("ninja");
+        signIn.click();
+
+        WebElement toDo = driver.findElement(By.className("new-todo"));
         toDo.sendKeys("Let's get started!");
         toDo.sendKeys(Keys.RETURN);
         assertThat(currentCounter(), equalTo("1"));
