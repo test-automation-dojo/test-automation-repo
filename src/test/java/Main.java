a7a0c98bdcd88679b970896748f5e4dd4a9f19ac
Adam Woods <adam.tomato@gmail.com>
2017-03-13 21:46:15 +0800

    cleanup local storage between tests

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,9 +1,5 @@
-import org.junit.AfterClass;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -14,16 +10,25 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class Main {
     private static WebDriver driver = new FirefoxDriver();
 
+    @BeforeClass
+    public static void setUpClass() {
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
     @AfterClass
-    public static void tearDown(){
+    public static void tearDownClass() {
         driver.quit();
+    }
+
+    @After
+    public void tearDown() {
+        ((JavascriptExecutor)driver).executeScript("window.localStorage.clear();");
     }
 
     @Test
     public void shouldAddToDos() throws Exception {
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
         driver.get("http://test-automation-dojo.com/todo");
+
         WebElement username = driver.findElement(By.id("username"));
         WebElement password = driver.findElement(By.id("password"));
         WebElement signIn = driver.findElement(By.id("sign-in"));
