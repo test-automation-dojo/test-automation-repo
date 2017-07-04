b759a2ed2a2afb39df9eb44bfa1f6e01755553c9
torstenleibrich <torsten.leibrich@gmail.com>
2017-07-04 22:12:06 +0800

    Added pizza ingredient list in prep for builder pattern dojo.

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,4 +1,7 @@
-import org.junit.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -29,7 +32,6 @@ public class Main {
     @Test
     public void shouldAddToDos() throws Exception {
         loginPage.navigate();
-
         loginPage.login();
         todoPage.addTodo("buy milk");
         todoPage.addTodo("buy vegemite");
@@ -37,6 +39,36 @@ public class Main {
 
         assertThat(todoPage.todoCount(), equalTo(3));
     }
+
+    @Test
+    public void shouldAddPizzaShoppingList() throws Exception {
+        loginPage.navigate();
+        loginPage.login();
+
+        String[] ingredients = {
+                "1/3 cup (80ml) passata",
+                "100g Hungarian salami",
+                "4 slices prosciutto, halved crossways",
+                "40g button mushrooms, thinly sliced",
+                "1/2 red capsicum, seeded, thinly sliced",
+                "1/4 cup (40g) pitted kalamata olives",
+                "4 bocconcini, coarsely torn",
+                "50g vine-ripened cherry tomatoes",
+                "Rocket leaves, to serve",
+                "Shaved parmesan, to serve",
+
+                "2 cups (300g) plain flour",
+                "1/2 teaspoon salt",
+                "2 teaspoon (1 sachet/7g) dried yeast",
+                "Pinch of caster sugar",
+        };
+
+        for (String ingredient : ingredients) {
+            todoPage.addTodo(ingredient);
+        }
+        assertThat(todoPage.todoCount(), equalTo(14));
+    }
+
 
     @Test
     public void shouldDeleteTodo() throws Exception {
